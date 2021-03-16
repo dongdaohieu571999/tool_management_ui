46b112e71fd89170813c6e0b9c0f6cfd965d91de
dongdaohieu <daohieudong571999@gmail.com>
2021-03-16 20:56:00 +0700

    update bang minh hoa

--- src/app/view/illustration/illustration-table/list-illustration/detail-illustration/create-illustration/create-illustration.component.ts
+++ src/app/view/illustration/illustration-table/list-illustration/detail-illustration/create-illustration/create-illustration.component.ts
@@ -9,7 +9,20 @@ export class CreateIllustrationComponent implements OnInit {
 
   constructor() { }
 
+  relatedPerson = new Array<any>();
+
   ngOnInit(): void {
+    this.relatedPerson.push({});
+  }
+
+  addField(){
+    this.relatedPerson.push({});
+  }
+
+  removeField(index: number){
+    if (index > -1) {
+      this.relatedPerson.splice(index, 1);
+    }
   }
 
 }
