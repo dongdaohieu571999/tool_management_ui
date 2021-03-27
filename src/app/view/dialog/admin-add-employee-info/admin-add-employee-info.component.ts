189037f36bbacb2f8bd6941195679f3680e86cd4
eechooo789 <62242038+eechooo789@users.noreply.github.com>
2021-03-27 18:03:50 +0700

    validate admin

--- src/app/view/dialog/admin-add-employee-info/admin-add-employee-info.component.ts
+++ src/app/view/dialog/admin-add-employee-info/admin-add-employee-info.component.ts
@@ -15,27 +15,32 @@ import { SnackbarService } from 'src/app/services/snackbar/snackbar.service';
 })
 export class AdminAddEmployeeInfoComponent implements OnInit {
 
-  constructor(public employeeService : EmployeeService,
-    @Inject(MAT_DIALOG_DATA) public employeinfoDTO: EmployeeInfoDTO,private common: CommonService,private router: Router,private notiService: SnackbarService) { }
+  constructor(public employeeService: EmployeeService,
+    @Inject(MAT_DIALOG_DATA) public employeinfoDTO: EmployeeInfoDTO, private common: CommonService, private router: Router, private notiService: SnackbarService) { }
 
   ngOnInit(): void {
 
   }
 
-  genders= Array[2] = [
-    {value: 'true', viewValue: 'Nam'},
-    {value: 'false', viewValue: 'Nữ'},
+  genders = Array[2] = [
+    { value: 'true', viewValue: 'Nam' },
+    { value: 'false', viewValue: 'Nữ' },
   ];
 
   marital_statuss = Array[2] = [
-    {value: 'true', viewValue: 'Đã Kết Hôn'},
-    {value: 'false', viewValue: 'Chưa Kết Hôn'},
+    { value: 'true', viewValue: 'Đã Kết Hôn' },
+    { value: 'false', viewValue: 'Chưa Kết Hôn' },
   ];
 
-  public onSubmit(){
-   this.employeeService.addEmployeeInfo(this.employeinfoDTO).subscribe((data =>{
-     this.employeeService.invokeRefreshTableFun();
-   }))
-    
-   }
+  public onSubmit() {
+    this.employeeService.addEmployeeInfo(this.employeinfoDTO).subscribe((data => {
+      this.employeeService.invokeRefreshTableFun();
+    }))
+
+  }
+
+  ageCheck: number;
+  checkAge: boolean;
+  
+
 }
