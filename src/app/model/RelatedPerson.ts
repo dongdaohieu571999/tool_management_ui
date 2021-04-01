a54ce7bd3cadf872dd002bea9661ee92a18595d9
dongdaohieu <daohieudong571999@gmail.com>
2021-04-01 10:09:46 +0700

    hjsdbc

--- src/app/model/RelatedPerson.ts
+++ src/app/model/RelatedPerson.ts
@@ -1,6 +1,7 @@
 import { Interest } from "./Interest";
 
 export class RelatedPerson{
+    id:number;
  full_name:string;
  relation:string;
  date_of_birth:Date;
@@ -8,15 +9,18 @@ export class RelatedPerson{
  carreer_group:number;
  listSubInterest:Array<Interest>;
 
- listSubInterestSelected:Array<Interest>;
-
- constructor(full_name:string,relation:string,date_of_birth:Date,gender:boolean,carreer_group:number,listSubInterest:Array<Interest>,listSubInterestSelected:Array<Interest>){
-     this.full_name = full_name;
-     this.relation = relation;
-     this.date_of_birth = date_of_birth;
-     this.gender = gender;
-     this.carreer_group = carreer_group;
-     this.listSubInterest = listSubInterest;
-     this.listSubInterestSelected = listSubInterestSelected;
- }
+    constructor(id:number,
+        full_name:string,
+        relation:string,
+        date_of_birth:Date,
+        gender:boolean,
+        carreer_group:number,
+        listSubInterest:Array<Interest>){
+            this.full_name = full_name;
+            this.relation = relation;
+            this.date_of_birth = date_of_birth;
+            this.gender = gender;
+            this.carreer_group = carreer_group;
+            this.listSubInterest = listSubInterest;
+    }
 }
--- src/app/view/illustration/illustration-table/list-illustration/detail-illustration/create-illustration/create-illustration.component.ts
+++ src/app/view/illustration/illustration-table/list-illustration/detail-illustration/create-illustration/create-illustration.component.ts
@@ -17,6 +17,7 @@ import { RefertableService } from 'src/app/services/refertable/refertable.servic
 import { SnackbarService } from 'src/app/services/snackbar/snackbar.service';
 import jsPDF from 'jspdf';
 import html2canvas from 'html2canvas'; 
+import { RelatedPersonService } from 'src/app/services/relatedPerson/related-person.service';
 
 @Component({
   selector: 'app-create-illustration',
@@ -25,7 +26,10 @@ import html2canvas from 'html2canvas';
 })
 export class CreateIllustrationComponent implements OnInit {
 
-  constructor(private snackBar:SnackbarService,private spinner:NgxSpinnerService,private referenceTable:RefertableService,private illustService : IllustrationService,private interest:InterestService,private activeRoute:ActivatedRoute,private common:CommonService,private customerService:CustomerService) { 
+  constructor(private relateSer:RelatedPersonService,private snackBar:SnackbarService,private spinner:NgxSpinnerService,
+    private referenceTable:RefertableService,private illustService : IllustrationService,
+    private interest:InterestService,private activeRoute:ActivatedRoute,
+    private common:CommonService,private customerService:CustomerService) { 
   }
 
   // biến dùng để xuất pdf
@@ -43,13 +47,16 @@ export class CreateIllustrationComponent implements OnInit {
   checkedTick = false;
 
   // disble button tính phí
-  checkCountPayment = true;
+  checkCountPayment = false;
 
   //hệ số định kỳ hàng năm
   mulPeriod:MultiplierForPaymentPeriod;
 
   //tổng số tiền bảo hiểm cuối cùng
   totalPayment=0;
+
+  // lấy id của illustration từ đường Link
+  id_ill:number;
   
 
 
@@ -149,7 +156,7 @@ for(let relate of this.relatedPerson){
   }
 
   addField(){
-    var relatedPer1 = new RelatedPerson('','',new Date(),true,0,[],[]);
+    var relatedPer1 = new RelatedPerson(0,'','',new Date(),true,0,[]);
     // deep copy this.subInterestList
     let list = this.subInterestList.map(x => Object.assign({},x));
     relatedPer1.listSubInterest = list
@@ -185,6 +192,7 @@ for(let relate of this.relatedPerson){
         this.customerInfo=data;
         this.illustrationMainInterest.full_name_insurance_buyer = this.customerInfo.full_name;
         this.illustrationMainInterest.id_illustration = params['id'];
+        this.id_ill = params['id'];
       }))
     })
   }
@@ -216,46 +224,92 @@ for(let relate of this.relatedPerson){
     window.print();
   }
 
-  save(){
-    this.illustration.illustrationSubInterestList = [];
-    for(let interest of this.subInterestListCopy){
-      if(!interest.isDisable){
-      this.activeRoute.queryParams.subscribe(params => {
+  // giá trị i tăng mỗi lần gọi đệ quy
+  i=0;
 
-        let subInterest = new IllustrationSubInterest(params['id'],interest.id,this.illustrationMainInterest.full_name_insured_person
-        ,this.illustrationMainInterest.insurance_buyer_relation_insured_person,this.illustrationMainInterest.birth_date_insured_person,this.calculateAge(this.illustrationMainInterest.birth_date_insured_person),
-        this.illustrationMainInterest.gender_insured_person,this.illustrationMainInterest.carrier_group_insured_person,interest.denominations,
-        interest.fee_value,false);
+  // hàm đệ quy
+  next(){
+    var relatePer = this.relatedPerson[this.i];
+    this.relateSer.addRelatedPerson({full_name:relatePer.full_name,
+      date_of_birth:relatePer.date_of_birth,gender:relatePer.gender,
+      carreer_group:relatePer.carreer_group,relation:relatePer.relation,
+    id_illustration:this.id_ill}).subscribe((data => {
 
-        
-        this.illustration.illustrationSubInterestList.push(subInterest);
-      })
-    }
-    }
 
-    for(let relatePer of this.relatedPerson){
       for(let interest of relatePer.listSubInterest){
         if(!interest.isDisable){
         this.activeRoute.queryParams.subscribe(params => {
           this.illustration.illustrationSubInterestList.push(new IllustrationSubInterest(params['id'],interest.id,relatePer.full_name
           ,relatePer.relation,relatePer.date_of_birth,this.calculateAge(relatePer.date_of_birth),relatePer.gender,relatePer.carreer_group,interest.denominations,interest.fee_value
-          ,true));
+          ,true,data));
         })
       }
       }
-    }
-    this.illustration.illustrationMainInterest.id_main_interest = this.mainInterestSelect.id;
-    this.illustration.id_customer_info = this.customerInfo.id;
-    this.illustration.illustrationMainInterest.age_insured_person = this.calculateAge(this.illustrationMainInterest.birth_date_insured_person);
-    
-    this.spinner.show();
+      // phải chạy đến phần tử cuối cùng của mảng thì mới cho phép lưu bảng minh họa
+      if(this.i == this.relatedPerson.length -1){
+        this.i=0;
+        this.processSaveIntoDB();
+                  } else {
+                    this.i++;
+                    this.next();
+                  }
 
-    this.illustService.saveOneIllustration(this.illustration).subscribe((data => {
-      this.snackBar.openSnackBar("Lưu Bảng Minh Họa Thành Công","Đóng");
-      this.spinner.hide();
-      this.checkedTick = false;
-      this.checkCountPayment = true;
-    }))
+
+    })) 
   }
 
+
+  processSaveIntoDB(){
+    this.illustration.illustrationMainInterest.id_main_interest = this.mainInterestSelect.id;
+              this.illustration.id_customer_info = this.customerInfo.id;
+              this.illustration.illustrationMainInterest.age_insured_person = this.calculateAge(this.illustrationMainInterest.birth_date_insured_person);
+              
+              
+          
+              this.illustService.saveOneIllustration(this.illustration).subscribe((data => {
+                this.snackBar.openSnackBar("Lưu Bảng Minh Họa Thành Công","Đóng");
+                this.spinner.hide();
+                this.checkedTick = false;
+                this.checkCountPayment = true;
+              }))
+  }
+
+  save(){
+    this.spinner.show();
+    this.illustration.illustrationSubInterestList = [];
+    // thêm người được bảo vệ bằng các quyền lợi bổ sung
+    this.relateSer.addRelatedPerson({full_name:this.illustrationMainInterest.full_name_insured_person,
+      date_of_birth:this.illustrationMainInterest.birth_date_insured_person,gender:this.illustrationMainInterest.gender_insured_person,
+      carreer_group:this.illustrationMainInterest.carrier_group_insured_person,relation:this.illustrationMainInterest.insurance_buyer_relation_insured_person,
+    id_illustration:this.id_ill}).subscribe((data => {
+      for(let interest of this.subInterestListCopy){
+        if(!interest.isDisable){
+        this.activeRoute.queryParams.subscribe(params => {
+  
+          let subInterest = new IllustrationSubInterest(params['id'],interest.id,this.illustrationMainInterest.full_name_insured_person
+          ,this.illustrationMainInterest.insurance_buyer_relation_insured_person,this.illustrationMainInterest.birth_date_insured_person,this.calculateAge(this.illustrationMainInterest.birth_date_insured_person),
+          this.illustrationMainInterest.gender_insured_person,this.illustrationMainInterest.carrier_group_insured_person,interest.denominations,
+          interest.fee_value,false,data);
+  
+          
+          this.illustration.illustrationSubInterestList.push(subInterest);
+        })
+      }
+      }
+      // lưu các người được bảo hiểm khác và lấy id của họ gắn vào cho mỗi illustrationSubInterest
+      // vì callback được gọi lồng nhau nên không thể dùng for loop mà ta cần dùng đệ quy
+      if(this.relatedPerson.length != 0){
+        this.next();
+      } else {
+        this.processSaveIntoDB();
+        
+      }
+      
+    }))
+  }
 }
+
+
+
+
+
