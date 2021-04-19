f37dbe6ad0bc06acc44c30862396fd4ae152a6d6
dongdaohieu <daohieudong571999@gmail.com>
2021-04-19 21:00:07 +0700

    fix ban than

--- src/app/services/common/common.service.ts
+++ src/app/services/common/common.service.ts
@@ -18,12 +18,12 @@ export class CommonService {
   titlePage:string;
 
   makeUrl(path: string): string{
-    // return 'http://localhost:8080/api'+path+'?token_key='+this.getCookie("token_key");
-    return 'http://35.225.118.49/api'+path+'?token_key='+this.getCookie("token_key");
+    return 'http://localhost:8080/api'+path+'?token_key='+this.getCookie("token_key");
+    // return 'http://35.225.118.49/api'+path+'?token_key='+this.getCookie("token_key");
   }
   makeUrlForCustomer(path: string): string{
-    // return 'http://localhost:8080/api'+path;
-    return 'http://35.225.118.49/api'+path;
+    return 'http://localhost:8080/api'+path;
+    // return 'http://35.225.118.49/api'+path;
   }
 
   public deleteCookie(name) {
